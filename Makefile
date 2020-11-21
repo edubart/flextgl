76a1a6ae66ddc79575b492bc6bf15cd3cafb6829
Eduardo Bart <edub4rt@gmail.com>
2020-11-21 09:56:28 -0300

    Add custom template for SDL2 in a single header

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	python flextGLgen.py -D generated/sdl_oneheader_gl33_core -t templates/sdl_oneheader profiles/gl33_core.txt
---
+++ generated/sdl_oneheader_gl33_core/flextgl.c
@@ -0,0 +1,10 @@
+/*
+    This file was generated using https://github.com/mosra/flextgl:
+
+        path/to/flextGLgen.py -D generated/sdl_oneheader_gl33_core -t templates/sdl_oneheader profiles/gl33_core.txt
+
+    Do not edit directly, modify the template or profile and regenerate.
+*/
+
+#define FLEXTGL_IMPL
+#include "flextGL.h"
