80bd6f7b6a37a92f10764392c36b3eae3610e02c
Eduardo Bart <edub4rt@gmail.com>
2020-12-07 21:51:30 -0300

    Single header lite GL Core 3.3 profile

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all:
 	python flextGLgen.py -D generated/sdl_oneheader_gl33_core -t templates/sdl_oneheader profiles/gl33_core.txt
+	python flextGLgen.py -D generated/lite_oneheader_gl33_core -t templates/lite_oneheader profiles/gl33_core.txt
---
+++ generated/lite_oneheader_gl33_core/flextGL.c
@@ -0,0 +1,10 @@
+/*
+    This file was generated using https://github.com/mosra/flextgl:
+
+        path/to/flextGLgen.py -D generated/lite_oneheader_gl33_core -t templates/lite_oneheader profiles/gl33_core.txt
+
+    Do not edit directly, modify the template or profile and regenerate.
+*/
+
+#define FLEXTGL_IMPL
+#include "flextGL.h"
